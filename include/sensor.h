526b4e38749d9ff006c91d2d5c49ccd37fc30c84
Matt Davis <matteius@gmail.com>
2024-11-15 19:28:51 -0500

    Cleanup

--- include/sensor.h
+++
@@ -1,9 +0,0 @@
-#ifndef SENSOR_H
-#define SENSOR_H
-
-#include <stdint.h>
-
-// Function to initialize the sensor
-int sensor_init();
-
-#endif // SENSOR_H
--- src/sensor.c
+++
@@ -1,8 +0,0 @@
-#include "sensor.h"
-#include <stdio.h>
-
-int sensor_init() {
-    printf("Initializing sensor...\n");
-    // Placeholder for sensor-specific initialization code
-    return 0;
-}
