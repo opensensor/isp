0138ef28eaa828a3daaa26d414b88ba3442068e2
Matt Davis <matteius@gmail.com>
2025-09-01 16:06:43 -0400

    Check pt

--- driver/tx_isp_proc.c
+++ driver/tx_isp_proc.c
@@ -6,8 +6,8 @@
 
 #define TX_ISP_PROC_JZ_DIR "jz"
 #define TX_ISP_PROC_ISP_DIR "isp"
-#define TX_ISP_PROC_ISP_W01_FILE "isp-ws0"
-#define TX_ISP_PROC_ISP_W02_FILE "isp-ws1"
+#define TX_ISP_PROC_ISP_W01_FILE "isp-W01"
+#define TX_ISP_PROC_ISP_W02_FILE "isp-W02"
 
 struct proc_context {
     struct proc_dir_entry *jz_dir;
