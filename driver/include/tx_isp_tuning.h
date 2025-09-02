bdc074c5f2e6208e78e67ba6e039b2da0a27c4f7
Matt Davis <matteius@gmail.com>
2025-09-01 22:24:03 -0400

    dif testing

--- driver/include/tx_isp_tuning.h
+++ driver/include/tx_isp_tuning.h
@@ -12,4 +12,6 @@ int isp_core_tuning_release(struct tx_isp_dev *dev);
 int isp_m0_chardev_release(struct inode *inode, struct file *file);
 int isp_m0_chardev_open(struct inode *inode, struct file *file);
 int tisp_code_create_tuning_node(void);
+int tisp_code_destroy_tuning_node(void);
+
 #endif //__TX_ISP_TUNING_H__
