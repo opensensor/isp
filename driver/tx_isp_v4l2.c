f0c8b03e8809ac2e35b8cfd313a0985b75b61619
Matt Davis <matteius@gmail.com>
2025-09-05 01:43:50 -0400

    Try registering the sub probes

--- driver/tx_isp_v4l2.c
+++ driver/tx_isp_v4l2.c
@@ -684,6 +684,7 @@ static int tx_isp_create_v4l2_device(int channel)
     vdev->fops = &tx_isp_v4l2_fops;
     vdev->ioctl_ops = &tx_isp_v4l2_ioctl_ops;
     vdev->vfl_dir = VFL_DIR_RX;  /* Add this - indicates capture device */
+    vdev->release = video_device_release;  /* ADD THIS - REQUIRED! */
     /* Note: device_caps member not available in this kernel version */
     /* Capabilities are communicated via VIDIOC_QUERYCAP ioctl instead */
     vdev->lock = &dev->lock;
