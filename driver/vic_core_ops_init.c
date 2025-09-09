ffecb22ff2f03def7051ca8d50ae1ff6848f5d67
Matt Davis <matteius@gmail.com>
2025-09-09 18:31:14 -0400

    Check pt BN exporter

--- driver/vic_core_ops_init.c
+++ driver/vic_core_ops_init.c
@@ -10,33 +10,33 @@ int vic_core_ops_init(void* arg1, void* arg2)
     
     if (!(uintptr_t)arg1 || (uintptr_t)arg1 >= 0xfffff001)
     {
-        isp_printf(); // Fixed: macro with no parameters, removed 3 arguments;
+        isp_printf();
     }
-    
-    int32_t* $s1_1 = (int32_t*)((char*)arg1  + 0xd4); // Fixed void pointer arithmetic
-    int32_t $v0_2 = *($s1_1 + 0x128);
+
+    char* $s1_1 = (char*)arg1 + 0xd4;
+    int32_t $v0_2 = *(int32_t*)($s1_1 + 0x128);
     int32_t result;
-    
+
     if (!(uintptr_t)arg2)
     {
         result = 0;
-        
+
         if ($v0_2 != 2)
         {
-            tx_vic_disable_irq(); // Fixed: removed arguments for void function;
-            *((int32_t*)((char*)$s1_1 + 0x128)) = 2; // Fixed void pointer dereference
+            tx_vic_disable_irq();
+            *(int32_t*)($s1_1 + 0x128) = 2;
         }
     }
     else
     {
         result = 0;
-        
+
         if ($v0_2 != 3)
         {
-            tx_vic_enable_irq(); // Fixed: removed arguments for void function;
-            *((int32_t*)((char*)$s1_1 + 0x128)) = 3; // Fixed void pointer dereference
+            tx_vic_enable_irq();
+            *(int32_t*)($s1_1 + 0x128) = 3;
         }
     }
-    
+
     return result;
 }
