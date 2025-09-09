992146a9e11780f61d2a74ca9de9676054b5e898
Matt Davis <matteius@gmail.com>
2025-09-09 19:11:59 -0400

    Check pt BN exporter

--- driver/absFun.c
+++ driver/absFun.c
@@ -1,12 +1,9 @@
 #include "include/main.h"
 
-
-  int32_t absFun(int32_t arg1, int32_t arg2) __attribute__((pure))
-
+int32_t __attribute__((pure)) absFun(int32_t arg1, int32_t arg2)
 {
     if (arg2 >= arg1)
         return arg2 - arg1;
-    
+
     return arg1 - arg2;
 }
-
