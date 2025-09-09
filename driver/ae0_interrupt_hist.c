11956e0aa114f82a3d2ca862abe2dcaa777a8842
Matt Davis <matteius@gmail.com>
2025-09-09 19:13:48 -0400

    Check pt BN exporter

--- driver/ae0_interrupt_hist.c
+++ driver/ae0_interrupt_hist.c
@@ -1,14 +1,20 @@
 #include "include/main.h"
 
-
-  int32_t ae0_interrupt_hist()
-
+int32_t ae0_interrupt_hist()
 {
-    int32_t $s0 = (system_reg_read(0xa050) & 3) << 0xb;
+    int32_t $s0;
     int32_t $a0_1;
     int32_t $a2;
-    private_dma_cache_sync(0, $s0 + data_b2f48, 0x800, 0);
-    
+    int32_t var_38_10 = 1;
+    struct tisp_event var_40_26; /* Proper type for event structure */
+
+    $s0 = (system_reg_read(0xa050) & 3) << 0xb;
+
+    /* Call private_dma_cache_sync - but check function signature first */
+    /* If it takes no parameters as declared: */
+    private_dma_cache_sync();
+    /* If it should take parameters, update the declaration in functions.h */
+
     if (data_b0e10 != 1)
     {
         $a0_1 = data_b2f48;
@@ -19,11 +25,9 @@
         $a0_1 = data_b2f48;
         $a2 = 0;
     }
-    
-    tisp_ae0_get_hist($s0 + $a0_1, 1, $a2);
-    int32_t var_38_10 = 1;
-    void var_40_25;
+
+    tisp_ae0_get_hist((int32_t*)($s0 + $a0_1), 1, $a2);
     tisp_event_push(&var_40_26);
+
     return 2;
 }
-
--- driver/ae1_interrupt_hist.c
+++ driver/ae1_interrupt_hist.c
@@ -1,15 +1,25 @@
 #include "include/main.h"
 
-
-  int32_t ae1_interrupt_hist()
-
+int32_t ae1_interrupt_hist()
 {
-    int32_t $s0 = (system_reg_read(0xa850) & 3) << 0xb;
+    int32_t $s0;
     int32_t var_38 = 6;
-    void var_40;
-    return 2;
-    private_dma_cache_sync(0, $s0 + data_b2f60, 0x800, 0);
-    tisp_ae1_get_hist($s0 + data_b2f60);
-    tisp_event_push(&var_40);
-}
+    /* void var_40; - This is invalid, need a proper type */
+    struct tisp_event var_40; /* Assuming this is an event structure */
 
+    $s0 = (system_reg_read(0xa850) & 3) << 0xb;
+
+    /* Note: This return statement would exit early - is this intentional? */
+    /* return 2; */
+
+    /* If private_dma_cache_sync takes no arguments per the declaration */
+    private_dma_cache_sync();
+
+    /* Or if it should take arguments, update the function declaration in functions.h */
+    /* private_dma_cache_sync(0, $s0 + data_b2f60, 0x800, 0); */
+
+    tisp_ae1_get_hist((int32_t*)($s0 + data_b2f60));
+    tisp_event_push(&var_40);
+
+    return 2;
+}
