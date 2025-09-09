47a9b252423710c68bd00b54dce8dc6dc4fd370e
Matt Davis <matteius@gmail.com>
2025-09-09 19:15:46 -0400

    Check pt BN exporter

--- driver/ae0_interrupt_hist.c
+++ driver/ae0_interrupt_hist.c
@@ -6,14 +6,11 @@ int32_t ae0_interrupt_hist()
     int32_t $a0_1;
     int32_t $a2;
     int32_t var_38_10 = 1;
-    struct tisp_event var_40_26; /* Proper type for event structure */
+    char var_40_26[64]; /* Generic buffer for event data */
 
     $s0 = (system_reg_read(0xa050) & 3) << 0xb;
 
-    /* Call private_dma_cache_sync - but check function signature first */
-    /* If it takes no parameters as declared: */
     private_dma_cache_sync();
-    /* If it should take parameters, update the declaration in functions.h */
 
     if (data_b0e10 != 1)
     {
@@ -27,7 +24,7 @@ int32_t ae0_interrupt_hist()
     }
 
     tisp_ae0_get_hist((int32_t*)($s0 + $a0_1), 1, $a2);
-    tisp_event_push(&var_40_26);
+    tisp_event_push(var_40_26);
 
     return 2;
 }
