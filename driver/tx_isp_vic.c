74ecc8b84062333fa6a4b719a8c0db47f1a60033
Matt Davis <matteius@gmail.com>
2025-09-12 01:37:06 -0400

    go forward

--- driver/tx_isp_vic.c
+++ driver/tx_isp_vic.c
@@ -2158,9 +2158,9 @@ int vic_core_s_stream(struct tx_isp_subdev *sd, int enable)
                     if (ourISPdev && ourISPdev->csi_dev) {
                         pr_info("vic_core_s_stream: Triggering CSI initialization for streaming\n");
                         /* Call CSI sensor ops ioctl with command 0x200000c to trigger csi_core_ops_init */
-                        if (ourISPdev->csi_dev->ops && ourISPdev->csi_dev->ops->sensor && 
-                            ourISPdev->csi_dev->ops->sensor->ioctl) {
-                            ret = ourISPdev->csi_dev->ops->sensor->ioctl(ourISPdev->csi_dev, 0x200000c, NULL);
+                        if (ourISPdev->csi_dev->sd && ourISPdev->csi_dev->sd->ops && ourISPdev->csi_dev->sd->ops->sensor && 
+                            ourISPdev->csi_dev->sd->ops->sensor->ioctl) {
+                            ret = ourISPdev->csi_dev->sd->ops->sensor->ioctl(ourISPdev->csi_dev, 0x200000c, NULL);
                             pr_info("vic_core_s_stream: CSI sensor ioctl returned %d\n", ret);
                         }
                     }
