b1d7f1881f230ca13e170ef124b10b7906ac3670
Matt Davis <matteius@gmail.com>
2025-09-14 16:42:23 -0400

    Interrupts were working prior commit

--- driver/tx_isp_core.c
+++ driver/tx_isp_core.c
@@ -4309,3 +4309,65 @@ int isp_trigger_frame_data_transfer(struct tx_isp_dev *dev)
     return -ETIMEDOUT;
 }
 EXPORT_SYMBOL(isp_trigger_frame_data_transfer);
+
+/* Frame done tracking variables */
+static atomic64_t frame_done_cnt = ATOMIC64_INIT(0);
+static int frame_done_cond = 0;
+static DECLARE_WAIT_QUEUE_HEAD(frame_done_wait);
+
+/**
+ * isp_frame_done_wakeup - Notify ISP core that a frame is ready
+ * This function is called by VIC when a frame is complete
+ * Based on Binary Ninja decompilation
+ */
+void isp_frame_done_wakeup(void)
+{
+    /* Increment frame done counter */
+    atomic64_inc(&frame_done_cnt);
+
+    /* Set condition flag */
+    frame_done_cond = 1;
+
+    /* Wake up any processes waiting for frame completion */
+    wake_up(&frame_done_wait);
+
+    pr_debug("*** ISP FRAME DONE WAKEUP: Frame %lld ready for processing ***\n",
+             atomic64_read(&frame_done_cnt));
+}
+EXPORT_SYMBOL(isp_frame_done_wakeup);
+
+/**
+ * isp_frame_done_wait - Wait for frame completion
+ * This function allows processes to wait for frame done events
+ */
+int isp_frame_done_wait(int timeout_ms)
+{
+    int ret;
+
+    /* Wait for frame done condition with timeout */
+    ret = wait_event_timeout(frame_done_wait,
+                            frame_done_cond,
+                            msecs_to_jiffies(timeout_ms));
+
+    if (ret > 0) {
+        /* Frame done occurred, clear condition */
+        frame_done_cond = 0;
+        return 0;
+    } else if (ret == 0) {
+        /* Timeout */
+        return -ETIMEDOUT;
+    }
+
+    return ret;
+}
+EXPORT_SYMBOL(isp_frame_done_wait);
+
+/**
+ * isp_frame_done_get_count - Get current frame done count
+ */
+uint64_t isp_frame_done_get_count(void)
+{
+    return atomic64_read(&frame_done_cnt);
+}
+EXPORT_SYMBOL(isp_frame_done_get_count);
+
