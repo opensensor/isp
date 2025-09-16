fc803098348c0885d9fadf730ea789de32bb7e59
Matt Davis <matteius@gmail.com>
2025-09-16 10:48:36 -0400

    more work on tuning

--- driver/tx_isp_tuning.c
+++ driver/tx_isp_tuning.c
@@ -1762,26 +1762,57 @@ int isp_core_tunning_unlocked_ioctl(struct file *file, unsigned int cmd, void __
                             extern struct tx_isp_dev *ourISPdev;
 
                             /* 1. AE (Auto Exposure) Updates - WITH NULL CHECKS */
+                            pr_info("*** TUNING DEBUG: Starting AE updates ***");
                             extern int tisp_tgain_update(void);
                             extern int tisp_again_update(void);
                             extern int tisp_ev_update(void);
                             extern int tisp_ae_ir_update(void);
 
+                            pr_info("*** TUNING DEBUG: About to call tisp_tgain_update ***");
                             int ae_ret = 0;
                             if (tisp_tgain_update) ae_ret = tisp_tgain_update();
-                            if (ae_ret == 0 && tisp_again_update) ae_ret = tisp_again_update();
-                            if (ae_ret == 0 && tisp_ev_update) ae_ret = tisp_ev_update();
-                            if (ae_ret == 0 && tisp_ae_ir_update) ae_ret = tisp_ae_ir_update();
+                            pr_info("*** TUNING DEBUG: tisp_tgain_update completed: %d ***", ae_ret);
+
+                            if (ae_ret == 0 && tisp_again_update) {
+                                pr_info("*** TUNING DEBUG: About to call tisp_again_update ***");
+                                ae_ret = tisp_again_update();
+                                pr_info("*** TUNING DEBUG: tisp_again_update completed: %d ***", ae_ret);
+                            }
+
+                            if (ae_ret == 0 && tisp_ev_update) {
+                                pr_info("*** TUNING DEBUG: About to call tisp_ev_update ***");
+                                ae_ret = tisp_ev_update();
+                                pr_info("*** TUNING DEBUG: tisp_ev_update completed: %d ***", ae_ret);
+                            }
+
+                            if (ae_ret == 0 && tisp_ae_ir_update) {
+                                pr_info("*** TUNING DEBUG: About to call tisp_ae_ir_update ***");
+                                ae_ret = tisp_ae_ir_update();
+                                pr_info("*** TUNING DEBUG: tisp_ae_ir_update completed: %d ***", ae_ret);
+                            }
                             pr_debug("TUNING: AE updates completed: %d\n", ae_ret);
 
                             /* 2. AWB (Auto White Balance) Updates */
+                            pr_info("*** TUNING DEBUG: Starting AWB updates ***");
                             extern int tisp_ct_update(void);
                             extern int tisp_ccm_ct_update(void);
                             extern int tisp_ccm_ev_update(void);
 
+                            pr_info("*** TUNING DEBUG: About to call tisp_ct_update ***");
                             int awb_ret = tisp_ct_update();
-                            if (awb_ret == 0) awb_ret = tisp_ccm_ct_update();
-                            if (awb_ret == 0) awb_ret = tisp_ccm_ev_update();
+                            pr_info("*** TUNING DEBUG: tisp_ct_update completed: %d ***", awb_ret);
+
+                            if (awb_ret == 0) {
+                                pr_info("*** TUNING DEBUG: About to call tisp_ccm_ct_update ***");
+                                awb_ret = tisp_ccm_ct_update();
+                                pr_info("*** TUNING DEBUG: tisp_ccm_ct_update completed: %d ***", awb_ret);
+                            }
+
+                            if (awb_ret == 0) {
+                                pr_info("*** TUNING DEBUG: About to call tisp_ccm_ev_update ***");
+                                awb_ret = tisp_ccm_ev_update();
+                                pr_info("*** TUNING DEBUG: tisp_ccm_ev_update completed: %d ***", awb_ret);
+                            }
                             pr_debug("TUNING: AWB/CCM updates completed: %d\n", awb_ret);
 
                             /* 3. Gamma Correction Updates */
