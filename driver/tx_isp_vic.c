decbd0ec5e121c79830903700a856af6ce1520bb
Matt Davis <matteius@gmail.com>
2025-09-18 15:55:58 -0400

    check pt interrupts have bene flowing

--- driver/tx_isp_vic.c
+++ driver/tx_isp_vic.c
@@ -490,7 +490,7 @@ int vic_framedone_irq_function(struct tx_isp_vic_device *vic_dev)
                 }
 
                 extern int vic_frame_complete_buffer_management(struct tx_isp_vic_device *vic_dev, uint32_t buffer_addr);
-                vic_frame_complete_buffer_management(vic_dev, completed_buffer_addr);
+                vic_frame_complete_buffer_management(vic_dev, buffer_addr);
             }
         }
 
