fd616b87f902fa708ae40c0ef62bc54138c8ecea
Matt Davis <matteius@gmail.com>
2025-09-21 15:01:47 -0400

    match ISP ioctl better

--- driver/tx_isp_csi.c
+++ driver/tx_isp_csi.c
@@ -761,6 +761,11 @@ int tx_isp_csi_probe(struct platform_device *pdev)
     /* Binary Ninja: void* $s1_1 = arg1[0x16] */
     pdata = pdev->dev.platform_data;
 
+    /* CRITICAL FIX: Set host_priv to CSI device for Binary Ninja compatibility */
+    /* Binary Ninja expects CSI device at offset 0xd4 (host_priv field) */
+    tx_isp_set_subdev_hostdata(&csi_dev->sd, csi_dev);
+    pr_info("*** CSI PROBE: Set host_priv to csi_dev %p for Binary Ninja compatibility ***\n", csi_dev);
+
     /* Binary Ninja: tx_isp_subdev_init(arg1, $v0, &csi_subdev_ops) */
     ret = tx_isp_subdev_init(pdev, &csi_dev->sd, &csi_subdev_ops);
     if (ret != 0) {
