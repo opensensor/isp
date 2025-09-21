a3f38cf2fba4c205c934256362a7abf4420a09d1
Matt Davis <matteius@gmail.com>
2025-09-21 11:37:34 -0400

    Debug interrupts

--- driver/tx-isp-module.c
+++ driver/tx-isp-module.c
@@ -1636,8 +1636,9 @@ irqreturn_t isp_vic_interrupt_service_routine(int irq, void *dev_id)
         return IRQ_NONE;
     }
 
-    /* Get VIC interrupt enable flag at offset +0x13c */
-    vic_irq_enable_flag = (uint32_t*)((char*)vic_dev + 0x13c);
+    /* CRITICAL FIX: Use safe struct member access instead of dangerous raw pointer arithmetic */
+    /* The offset 0x13c was causing kernel panics - use proper struct member */
+    vic_irq_enable_flag = &vic_dev->irq_enabled;
 
     /* Binary Ninja: int32_t $v1_7 = not.d(*($v0_4 + 0x1e8)) & *($v0_4 + 0x1e0) */
     /* Binary Ninja: int32_t $v1_10 = not.d(*($v0_4 + 0x1ec)) & *($v0_4 + 0x1e4) */
