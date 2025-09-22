0768014794882333dd5735f5ca8763b023bd73fd
Matt Davis <matteius@gmail.com>
2025-09-22 16:50:57 -0400

    More IRQ debug work

--- driver/tx_isp_subdev.c
+++ driver/tx_isp_subdev.c
@@ -572,8 +572,8 @@ void tx_isp_subdev_auto_link(struct platform_device *pdev, struct tx_isp_subdev 
     pr_info("*** DEBUG: Device name comparison - checking '%s' ***\n", dev_name);
     pr_info("*** DEBUG: About to check device name matches ***\n");
 
-    if (strcmp(dev_name, "tx-isp-csi") == 0) {
-        /* Link CSI device */
+    if (strcmp(dev_name, "isp-w00") == 0) {
+        /* Link CSI device - device name is now "isp-w00" */
         struct tx_isp_csi_device *csi_dev = container_of(sd, struct tx_isp_csi_device, sd);
         ourISPdev->csi_dev = csi_dev;
         if (sd->regs) {
