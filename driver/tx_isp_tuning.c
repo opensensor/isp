fd54827957686b2d4f2726d8b0f4fb0917efd663
Matt Davis <matteius@gmail.com>
2025-09-24 20:06:50 -0400

    check pt

--- driver/tx_isp_tuning.c
+++ driver/tx_isp_tuning.c
@@ -2054,7 +2054,7 @@ int tisp_init(void *sensor_info, char *param_name)
     pr_info("*** WRITING VIC INTERRUPT ENABLE REGISTERS ***\n");
 
     /* Configure VIC interrupt registers - EXACTLY like working logs */
-    if (isp_dev->vic_dev && isp_dev->vic_dev->vic_regs) {
+    if (ourISPdev->vic_dev && isp_dev->vic_dev->vic_regs) {
         void __iomem *vic_regs = isp_dev->vic_dev->vic_regs;
 
         /* CRITICAL FIX: Based on Binary Ninja analysis of interrupt handler */
