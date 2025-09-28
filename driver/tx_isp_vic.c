a013fd650a2de1f1ccd92b3b3f40484f3afd56e1
Matt Davis <matteius@gmail.com>
2025-09-28 13:26:01 -0400

    Try again for continuous irqs

--- driver/tx_isp_vic.c
+++ driver/tx_isp_vic.c
@@ -2901,7 +2901,7 @@ int vic_core_s_stream(struct tx_isp_subdev *sd, int enable)
 
 /* Define VIC video operations */
 static struct tx_isp_subdev_video_ops vic_video_ops = {
-    .s_stream = vic_core_s_stream,
+    .s_stream = NULL,
     .link_stream = NULL,  /* CRITICAL FIX: tx_isp_video_link_stream calls link_stream! */
 };
 
