be0d81540015232ce990663098a8775edc673e99
Matt Davis <matteius@gmail.com>
2025-09-28 02:01:34 -0400

    probably will break

--- driver/include/tx-isp-device.h
+++ driver/include/tx-isp-device.h
@@ -639,34 +639,6 @@ void *get_frame_channel_device_ptr(int channel)
 
 #define FRAME_CHANNEL_MAGIC 0xDEADBEEF
 
-/* Subdevice type definitions */
-enum tx_isp_subdev_type {
-    TX_ISP_SUBDEV_TYPE_SOURCE = 1,  /* Source devices (CSI, VIN, FS) */
-    TX_ISP_SUBDEV_TYPE_SINK = 2,    /* Sink devices (VIC, CORE) */
-};
-
-
-/* Subdevice descriptor - cleaner than Binary Ninja struct */
-struct tx_isp_subdev_desc {
-    const char *name;
-    enum tx_isp_subdev_type type;
-    uint32_t device_id;
-    uint32_t src_index;
-    uint32_t dst_index;
-    struct platform_device *pdev;
-    const struct file_operations *fops;
-    bool create_misc_device;
-    bool create_proc_entry;
-};
-
-/* Subdevice runtime data */
-struct tx_isp_subdev_runtime {
-    struct tx_isp_subdev_desc *desc;
-    struct miscdevice *misc_dev;
-    struct proc_dir_entry *proc_entry;
-    void *driver_data;
-    bool initialized;
-};
 
 /*
  * Internal ops. Never call this from drivers, only the tx isp device can call
