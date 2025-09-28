d89d50f60edfc77e55187d3be9d8b58c53be3143
Matt Davis <matteius@gmail.com>
2025-09-28 11:40:43 -0400

    Try again for continuous irqs

--- driver/tx-isp-module.c
+++ driver/tx-isp-module.c
@@ -6650,6 +6650,53 @@ EXPORT_SYMBOL(system_reg_write_clm);
 EXPORT_SYMBOL(system_reg_write_gb);
 EXPORT_SYMBOL(system_reg_write_gib);
 
+/* Wake up waiters when frame is ready - matches reference driver pattern */
+static void frame_channel_wakeup_waiters(struct frame_channel_device *fcd)
+{
+    unsigned long flags;
+
+    if (!fcd) {
+        return;
+    }
+
+    pr_debug("Channel %d: Waking up frame waiters\n", fcd->channel_num);
+
+    /* Mark frame as ready and wake up waiters */
+    spin_lock_irqsave(&fcd->state.buffer_lock, flags);
+    fcd->state.frame_ready = true;
+    spin_unlock_irqrestore(&fcd->state.buffer_lock, flags);
+
+    /* Wake up any threads waiting for frame completion */
+    wake_up_interruptible(&fcd->state.frame_wait);
+
+    pr_debug("Channel %d: Frame ready\n", fcd->channel_num);
+}
+
+/* Public function to wake up all streaming frame channels - for tuning system */
+void tx_isp_wakeup_frame_channels(void)
+{
+    int i;
+
+    pr_debug("*** Waking up all streaming frame channels ***\n");
+
+    for (i = 0; i < num_channels; i++) {
+        struct frame_channel_device *fcd = &frame_channels[i];
+        if (fcd && fcd->state.streaming) {
+            unsigned long flags;
+
+            /* Mark frame as ready and wake up waiters */
+            spin_lock_irqsave(&fcd->state.buffer_lock, flags);
+            if (!fcd->state.frame_ready) {
+                fcd->state.frame_ready = true;
+                wake_up_interruptible(&fcd->state.frame_wait);
+                pr_debug("*** Woke up channel %d for frame processing ***\n", i);
+            }
+            spin_unlock_irqrestore(&fcd->state.buffer_lock, flags);
+        }
+    }
+}
+EXPORT_SYMBOL(tx_isp_wakeup_frame_channels);
+
 /* Export platform devices for tx_isp_core.c to reference */
 EXPORT_SYMBOL(tx_isp_csi_platform_device);
 EXPORT_SYMBOL(tx_isp_vic_platform_device);
